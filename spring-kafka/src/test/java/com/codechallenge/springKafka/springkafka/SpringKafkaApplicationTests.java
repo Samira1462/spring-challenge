c9de7407f638dc4d886802eda78b9a4bad83c8d5
samira <Samir.Radmaneshfar@gmail.com>
2023-08-04 20:48:40 +0200

    Initial commit

---
+++ spring-kafka/src/test/java/com/codechallenge/springKafka/springkafka/SpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallenge.springKafka.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
